5ce97e127b309b0481f1fa8b82930633dec50289
theonlydude <theonlydude@server.fake>
2026-08-05 08:39:35 +0200

    web: cassc javascript module url has changed on github, update it

--- web/pull_latest_tracker.sh
+++ web/pull_latest_tracker.sh
@@ -1,7 +1,7 @@
 echo "begin tracker pull"
 cd $(dirname $0)
 
-NEW_RELEASE=$(curl -s https://api.github.com/repos/chriscauley/super-metroid/releases/latest | jq -r '.assets[0].browser_download_url')
+NEW_RELEASE=$(curl -s https://api.github.com/repositories/574542089/releases/latest | jq -r '.assets[0].browser_download_url')
 
 rm -rf client
 echo "${NEW_RELEASE}"
